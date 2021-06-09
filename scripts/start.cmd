d98ab0349bcb2513e090f700dbf19e16b2c2417a
voronindim <VoronindimTvink123@mail.ru>
2021-06-09 17:32:29 +0300

    Исправил замечания #1

--- scripts/start.cmd
+++ scripts/start.cmd
@@ -1,6 +1,8 @@
 SET PATH_TO_APP=%~dp0..\Valuator\
 SET PATH_TO_NGINX=%~dp0..\nginx\ 
 
+cd %PATH_TO_APP%
+dotnet build
 start /d %PATH_TO_APP% dotnet run --no-build --urls "http://localhost:5001"
 start /d %PATH_TO_APP% dotnet run --no-build --urls "http://localhost:5002"
                                                                            
