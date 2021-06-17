daa54803c81667bebd4b271d70c1e7948eb5890f
voronindim <VoronindimTvink123@mail.ru>
2021-06-17 18:53:55 +0300

    Реализована 6 лабораторная работа

---
+++ Chat/Client/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Text.Json;
+
+namespace Client
+{
+    class Program
+    {
+        public static void StartClient(string address, int port, string message)
+        {
+            try
+            {
+                IPAddress ipAddress = address == "localhost" ? IPAddress.Loopback : IPAddress.Parse(address);
+                IPEndPoint remoteEP = new IPEndPoint(ipAddress, port);
+
+                // CREATE
+                Socket sender = new Socket(
+                    ipAddress.AddressFamily,
+                    SocketType.Stream,
+                    ProtocolType.Tcp);
+
+                try
+                {
+                    // CONNECT
+                    sender.Connect(remoteEP);
+
+                    // SEND
+                    int bytesSent = sender.Send(Encoding.UTF8.GetBytes(message));
+
+                    // RECEIVE
+                    byte[] buf = new byte[1024];
+                    int bytesRec = sender.Receive(buf);
+                    var history = JsonSerializer.Deserialize<List<string>>(Encoding.UTF8.GetString(buf, 0, bytesRec));
+                    
+                    foreach (var msg in history)
+                    {
+                        Console.WriteLine(msg);
+                    }
+
+                    // RELEASE
+                    sender.Shutdown(SocketShutdown.Both);
+                    sender.Close();
+
+                }
+                catch (ArgumentNullException ane)
+                {
+                    Console.WriteLine("ArgumentNullException : {0}", ane.ToString());
+                }
+                catch (SocketException se)
+                {
+                    Console.WriteLine("SocketException : {0}", se.ToString());
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Unexpected exception : {0}", e.ToString());
+                }
+
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            catch (ArgumentOutOfRangeException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            StartClient(args[0], Int32.Parse(args[1]), args[2]);
+        }
+    }
+}
---
+++ Chat/Server/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Text.Json;
+
+namespace Server
+{
+    class Program
+    {
+        private static List<string> _history = new List<string>();
+        public static void StartListening(int port)
+        {
+            // Привязываем сокет ко всем интерфейсам на текущей машинe
+            IPAddress ipAddress = IPAddress.Any;
+
+            IPEndPoint localEndPoint = new IPEndPoint(ipAddress, port);
+
+            // CREATE
+            Socket listener = new Socket(
+                ipAddress.AddressFamily,
+                SocketType.Stream,
+                ProtocolType.Tcp);
+
+            try
+            {
+                // BIND
+                listener.Bind(localEndPoint);
+
+                // LISTEN
+                listener.Listen(10);
+
+                while (true)
+                {
+                    // ACCEPT
+                    Socket handler = listener.Accept();
+
+                    byte[] buf = new byte[1024];
+
+                    // RECEIVE
+                    int bytesRec = handler.Receive(buf);
+                    string data = Encoding.UTF8.GetString(buf, 0, bytesRec);
+
+                    _history.Add(data);
+                    Console.WriteLine("Message received: {0}", data);
+
+                    // Отправляем текст обратно клиенту
+                    var jsonString = JsonSerializer.Serialize(_history);
+                    byte[] msg = Encoding.UTF8.GetBytes(jsonString);
+
+                    // SEND
+                    handler.Send(msg);
+
+                    // RELEASE
+                    handler.Shutdown(SocketShutdown.Both);
+                    handler.Close();
+                }
+
+            }            
+            catch (ArgumentOutOfRangeException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+            
+        }
+        static void Main(string[] args)
+        {
+            StartListening(Int32.Parse(args[0]));
+        }
+    }
+}
